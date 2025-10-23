eabf46a2edc7e4e6e9917dd1a82518835b915458
pmalviya12 <malviyapragati43@gmail.com>
2025-10-23 19:19:40 +0530

    Task3: added validation messages

--- app/controllers/company/jobs_controller.rb
+++ app/controllers/company/jobs_controller.rb
@@ -11,6 +11,11 @@ class Company::JobsController < ApplicationController
 
   def create
     @job = current_company.jobs.new(job_params)
+    if @job.save
+      redirect_to jobs_path
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def show
